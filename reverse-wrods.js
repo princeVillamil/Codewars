cceb740a33f9c562be1c2cec0f995db035076fa1
JaMox2 <jeffreyvillamilwork@gmail.com>
2022-07-05 16:15:27 +0800

    7s

---
+++ reverse-wrods.js
@@ -0,0 +1,11 @@
+/*
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+Examples
+
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  s
+*/
+function reverseWords(str) {
+    // Go for it
+    return str.split(' ').map(x=>x.split('').reverse().join('')).join(' ')
+  }
