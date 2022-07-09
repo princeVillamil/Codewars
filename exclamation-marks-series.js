72ddd6274f3fbf7b7c9fa40d6d01fe80be095a32
JaMox2 <jeffreyvillamilwork@gmail.com>
2022-07-09 20:51:22 +0800

    8s

--- exclamation-marks-series.js
+++ exclamation-marks-series.js
@@ -14,3 +14,9 @@ function replace(s){
     return s.replace(/[aeiou]/ig, "!")
     
 }
+
+// Repeat
+
+function remove (string) {
+    return string[string.length-1] == '!' ? string.slice(0,string.length-1) : string
+  }
