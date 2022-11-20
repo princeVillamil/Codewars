57918c7931a969015f0ebe413741568b4ddd7346
princeVillamil <jeffreyvillamilwork@gmail.com>
2022-11-19 15:28:47 -1000

    ... you dont see this 6kyu

---
+++ replace-with-alphabet-postion.js
@@ -0,0 +1,33 @@
+/*
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+Example
+
+alphabetPosition("The sunset sets at twelve o' clock.")
+
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+*/
+function alphabetPosition(text) {
+  let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  return text.toLowerCase().split('').map(x=>{
+    if(x.charCodeAt(0) >= 97 && x.charCodeAt(0) <= 122){
+			return letters.indexOf(x) + 1
+    }else return null
+  }).filter(x=>x!=null).join(' ')
+}
+
+
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+Test.assertEquals(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+  });
+});
