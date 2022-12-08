97b122c288a32e011c8e3e498c532d01ef8bc6b5
princeVillamil <jeffreyvillamilwork@gmail.com>
2022-12-08 10:36:30 +0800

    day 4 done

--- ADVENT OF CODE- Day 4 - camp cleanup.js
+++ ADVENT OF CODE- Day 4 - camp cleanup.js
@@ -86,6 +86,53 @@ test(bod)
 // "2-6,4-8"]
 // test(placeHolderArr)
 
+/*
+--- Part Two ---
+
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+    5-7,7-9 overlaps in a single section, 7.
+    2-8,3-7 overlaps all of the sections 3 through 7.
+    6-6,4-6 overlaps in a single section, 6.
+    2-6,4-8 overlaps in sections 4, 5, and 6.
+
+So, in this example, the number of overlapping assignment pairs is 4.
+
+In how many assignment pairs do the ranges overlap?
+
+Your puzzle answer was 905.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+*/
+function test(arr){
+  return arr.reduce((acc,pair,i)=>{
+    let isContained = false;
+    let [pairLeft, pairRight] = pair.split(',')
+    let [leftStart, leftEnd] = pairLeft.split('-')
+    let [rightStart, rightEnd] = pairRight.split('-')
+    
+    leftStart = parseInt(leftStart)
+    rightStart = parseInt(rightStart)
+    leftEnd = parseInt(leftEnd)
+    rightEnd = parseInt(rightEnd)
+    
+    let groupOne = leftEnd - leftStart
+    let groupTwo = rightEnd - rightStart
+    
+    if(groupOne>=groupTwo){
+      isContained = leftStart >= rightStart && leftStart<= rightEnd || leftEnd >= rightStart && leftStart<= rightEnd
+    }else{
+      isContained = rightStart >= leftStart && rightStart<= leftEnd || rightEnd >= leftStart && rightStart<= leftEnd
+    }
+    
+    return acc+(isContained?1:0)
+    
+  }, 0)
+}
+var bod = document.body.innerText.trim().split('\n')
+test(bod)
 
 
 var bod = `14-38,14-14
