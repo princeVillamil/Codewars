1ab1b67cf1b7f90d447c1d1ada0457af61340e28
princeVillamil <jeffreyvillamilwork@gmail.com>
2022-12-25 15:02:51 +0800

    8kyu

---
+++ fuel-calculator.js
@@ -0,0 +1,28 @@
+/*
+In this kata you will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+
+Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+
+Good Luck!
+Note
+
+1 Dollar = 100 Cents
+
+*/
+function fuelPrice(litres, pricePerLitre) {
+  for (var i = 2; i <= 10; i +=2) { //discount loop
+    if (litres >= i) {
+      pricePerLitre -= 0.05;
+    }
+  }
+  return Math.round(litres*pricePerLitre * 100) / 100
+}
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(fuelPrice(5, 1.23), 5.65);
+Test.assertEquals(fuelPrice(8, 2.5), 18.40);
+Test.assertEquals(fuelPrice(5, 5.6), 27.50);
+  });
+});
