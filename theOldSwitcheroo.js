1ae5649ad0415e9b51885895d66ba98e13ff19a8
princeVillamil <jeffreyvillamilwork@gmail.com>
2023-02-07 18:31:33 +0800

    7kyu

---
+++ theOldSwitcheroo.js
@@ -0,0 +1,27 @@
+/*
+Write a function
+
+vowel2index(str)
+
+that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+E.g:
+
+vowel2index('this is my string') == 'th3s 6s my str15ng'
+vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel2index('') == ''
+
+Your function should be case insensitive to the vowels.
+*/
+function vowel2index(str) {
+  let vowels = ['a','e','i','o','u']
+  return str = str.split('').map((x,i)=>{
+    if(vowels.includes(x)) return x = `${i+1}`
+    else return x
+  }).join('')
+}
+describe('Initial test',() => {
+  Test.assertEquals(vowel2index('this is my string'),'th3s 6s my str15ng');
+  Test.assertEquals(vowel2index('Codewars is the best site in the world'),'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld');
+  Test.assertEquals(vowel2index('Tomorrow is going to be raining'), 'T2m4rr7w 10s g1415ng t20 b23 r2627n29ng');
+  Test.assertEquals(vowel2index(''), '');
+});
